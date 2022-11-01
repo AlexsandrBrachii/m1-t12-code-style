ac7e2974dceb6c37497c9a6c8d12a643d79f695f
nicknaz <57040971+nicknaz@users.noreply.github.com>
2022-11-01 21:41:06 +0300

    Update DepositCalculator.java
    Привет)
    По формату и отступам всё хорошо, но недоглядел наименования некоторых переменных

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,22 +1,25 @@
 public class DepositCalculator {
     double calculateComplexPercent (double a, double y, int d) {
+        #в задании по кодстайлу просили поправить наименования переменных в данном случае amount, yearRate, depositPeriod
         double pay = a * Math.pow((1 + y / 12), 12 * d);
         return getRound(pay, 2);
     }
 
     double calculateSimplePercent (double doubleAmount, double double_year_rate, int deposit_period) {
+        #здесь так же amount, yearRate, depositPeriod
+        #doubleAmount меняем на amount так как тип переменной в ее названии указывать не надо
         return getRound(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
     }
 
     double getRound (double value, int places) {
-        double ScaLe = Math.pow(10, places);
+        double ScaLe = Math.pow(10, places); # ScaLe -> scale
         return Math.round(value * ScaLe) / ScaLe;
     }
 
     void doImportantJob() {
         int period;
         int action;
-        Scanner abcdef = new Scanner(System.in);
+        Scanner abcdef = new Scanner(System.in); # abcdef -> scanner
 
         System.out.println("Введите сумму вклада в рублях:");
         int amount = abcdef.nextInt();
@@ -27,7 +30,7 @@ public class DepositCalculator {
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = abcdef.nextInt();
 
-        double outDoubleVar = 0;
+        double outDoubleVar = 0; # outDoubleVar -> out
 
         if (action == 1) {
             outDoubleVar = calculateSimplePercent(amount, 0.06, period);
